50fa389f25085174881f153753e755698c13a7d0
qwang1990 <wangqi2179@163.com>
2016-04-06 20:08:04 +0800

    remove unused comments

--- Dozer-learn/src/main/java/com/zeus/MappingTrigger.java
+++ Dozer-learn/src/main/java/com/zeus/MappingTrigger.java
@@ -1,22 +1,9 @@
 package com.zeus;
 
-import com.zeus.domain.DestinationObject;
-import com.zeus.object.SourceObject;
-import com.zeus.object.SubItem;
-import org.dozer.DozerBeanMapper;
-import org.dozer.Mapper;
-import org.dozer.metadata.ClassMappingMetadata;
-import org.dozer.metadata.MappingMetadata;
-
-import java.util.*;
-
 /**
  * Created by wangqi on 16/4/6.
  */
 public class MappingTrigger {
-
-
-
     public static void main(String[] args) {
 
     }
