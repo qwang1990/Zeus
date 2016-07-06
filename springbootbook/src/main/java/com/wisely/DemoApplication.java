19bd0555f6175e5b4fdbdbb146d0a54bc4259b94
qwang1990 <wangqi2179@163.com>
2016-07-06 18:14:14 +0800

    fix

--- springbootbook/src/main/java/com/wisely/DemoApplication.java
+++ springbootbook/src/main/java/com/wisely/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import service.HelloService;
 
 import java.io.IOException;
 
@@ -22,20 +21,12 @@ public class DemoApplication {
     @Autowired
     private AuthorSettings authorSettings;
 
-    @Autowired
-    HelloService helloService;
+
 
 	@RequestMapping(value = "/")
     String index() throws IOException, DocumentException {
         return "hello world "+authorSettings.getName() + " age: "+authorSettings.getAge();
     }
-
-    @RequestMapping(value = "/hello")
-    String hello() throws IOException, DocumentException {
-        return helloService.sayHello();
-    }
-
-
 
 
 	public static void main(String[] args) {
--- testauto/src/main/java/TestAutoconfig.java
+++
@@ -1,8 +0,0 @@
-
-
-/**
- * Created by wangqi on 16/7/5.
- */
-public class TestAutoconfig {
-
-}
